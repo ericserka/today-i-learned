fb29c949de6152adb4e97b7d32d66e5fc108e5b2
Eric Serka <45241755+ericserka@users.noreply.github.com>
2023-05-02 14:21:27 -0300

    Create indexedAccessTypes.ts

---
+++ Typescript/indexedAccessTypes.ts
@@ -0,0 +1,26 @@
+function query<T>(
+    items: T[],
+    query: {
+        [TProp in keyof T]?: (val: T[TProp]) => boolean
+    }
+) {
+    return items.filter(item => {
+        for (const property of Object.keys(item)) {
+            const propertyQuery = query[property]
+            if (propertyQuery && propertyQuery(item[property])) {
+                return true
+            }
+        }
+        return false
+    })
+}
+
+const matches = query(
+    [
+        { name: "Ted", age: 12 },
+        { name: "Angie", age: 31 }
+    ],
+    {
+        name: name => name === "Angie",
+        age: age => age > 30
+    })
